b8eb9201d4d5a5acfa4a17a3ba2b57d490dd4c01
jhoudu <jhoudu@gmail.com>
2021-01-19 19:59:57 +0100

    Amélioration DB init

--- initdb/b_ressources.sql
+++ initdb/b_ressources.sql
@@ -5,7 +5,7 @@
 
 BEGIN;
 
-SET client_encoding = 'LATIN1';
+SET client_encoding = 'UTF-8';
 
 create table api.ressources (
   id serial primary key,
@@ -20,7 +20,10 @@ create table api.ressources (
   usages text
 );
 
-insert into api.ressources (titre, lien) values
-  ('tuto 0 postgREST', 'https://postgrest.org/en/v7.0.0/tutorials/tut0.html');
-  
-  COMMIT;
+insert into api.ressources (parcours, titre, lien, description, datepub, source, cible, secteurs ) values
+  ('Collègues','tuto 0 postgREST', 'https://postgrest.org/en/v7.0.0/tutorials/tut0.html', 'Premier tutorial pour manipuler PostgREST','2021-01-18T22:13:29.828+00:00','Internet', null, 'Formation');
+insert into api.ressources (parcours, titre, lien, description, datepub, source, cible, secteurs ) values
+  ('Amis','Scotch', 'https://www.tiktok.com/@caarlwin/video/6915810594624916738?sender_device=pc&sender_web_id=6894615265109476869&is_from_webapp=v2', 'Premier contenu trouvé sur TikTok','2021-01-19T22:13:29.828+00:00','TikTok', 'Pegi 3', 'Humour');
+insert into api.ressources (parcours, titre, lien, description, datepub, source, cible, secteurs ) values
+  ('Famille','La danse des robots', 'https://youtu.be/fn3KWM1kuAw', 'Un lien émouvant',null,'Youtube', 'Pegi 12', 'Variétés,Humour');  
+COMMIT;
